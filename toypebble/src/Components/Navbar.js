879eb5b406d978bd0a23c2aca02ef72df9c6e718
unknown <kmrinandita@gmail.com>
2024-07-02 13:17:12 +0530

    Updated

--- toypebble/src/Components/Navbar.js
+++ toypebble/src/Components/Navbar.js
@@ -12,7 +12,7 @@ function MyNavbar({ backgroundColor }) {
   return (
     <Navbar collapseOnSelect expand="lg" className="navbar-custom" style={{ backgroundColor }}>
       <Container>
-        <Navbar.Brand href="#home"><img src='/assets/ToyPebble_Logo.png' alt='' width={80} height={80}/></Navbar.Brand>
+        <Navbar.Brand href="/"><img src='/assets/ToyPebble_Logo.png' alt='' width={80} height={80}/></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
@@ -20,12 +20,12 @@ function MyNavbar({ backgroundColor }) {
           </Nav>
           <Nav>
           <Nav.Link href="/#/subscription"> Subscription</Nav.Link>
-          <Nav.Link href="#deets"> My Orders</Nav.Link>
+          <Nav.Link href="/#/myOrders"> My Orders</Nav.Link>
             <Nav.Link href="/#/toys"> Toys</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
+            <Nav.Link eventKey={2} href="/#/ourStory">
               Our Story 
             </Nav.Link>
-            <Nav.Link href="#deets"> Contact</Nav.Link>
+            <Nav.Link href="#"> Contact</Nav.Link>
             <Nav.Link href="/#/login" style={{ border: '1px solid #FFADAD', backgroundColor: '#FFADAD', borderRadius: '32px'}}>
               Login/Sign Up 
             </Nav.Link>
